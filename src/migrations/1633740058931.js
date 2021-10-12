365ae83945bdd840fbecae9a6ef5be632c61da62
SplittyDev <splittydev@gmail.com>
2021-10-12 21:36:52 +0200

    Add settings migration

--- src/migrations/1633740058931.js
+++ src/migrations/1633740058931.js
@@ -3,6 +3,8 @@
 import MigrationManager from 'migrations/MigrationManager'
 import ModuleList from 'components/widgets'
 import { WidgetHelper } from 'helpers'
+import { isNil } from 'lodash'
+import { validate as isUuid } from 'uuid'
 import type { TWidget } from "../types/TWidget";
 
 type TAnyDict = {
@@ -16,15 +18,22 @@ type TAnyDict = {
  *  all widgets of those dashboards are patched with an instance id.
  *  The array-based widget list is then replaced by the new object-
  *  based widget-instance map and should load just fine.
+ *  If widget settings exist where the key is not a uuid
  */
 
 MigrationManager.registerMigration({
   version: 1633740058931,
   run() {
+
+    // Load state
     const recoilPersistRaw = localStorage.getItem('recoil-persist')
-    if (recoilPersistRaw === null) return
+    if (isNil(recoilPersistRaw)) return
+
+    // Parse state
     const recoilPersistStr: string = (recoilPersistRaw: any)
     const recoilPersist: TAnyDict = JSON.parse(recoilPersistStr)
+
+    // Migrate dashboards
     if ('dashboardsState' in recoilPersist) {
       const convertibleDashboards = recoilPersist.dashboardsState
         .filter(dashboard => 'widgets' in dashboard)
@@ -41,6 +50,27 @@ MigrationManager.registerMigration({
         recoilPersist.dashboardsState[dashboardIndex].widgets = instancedWidgets
       }
     }
+
+    // Migrate widget settings
+    if ('widgetSettingsState' in recoilPersist) {
+      const convertibleSettingKeys = Object
+        .keys(recoilPersist.widgetSettingsState)
+        .filter(key => !isUuid(key))
+      for (const widgetId of convertibleSettingKeys) {
+        const originalSettings = recoilPersist.widgetSettingsState[widgetId]
+        delete recoilPersist.widgetSettingsState[widgetId]
+        const matchingDashboards = recoilPersist.dashboardsState
+          .filter(dashboard => Object.keys(dashboard.widgets).includes(widgetId))
+        for (const dashboard of matchingDashboards) {
+          const matchingInstanceIds = dashboard.widgets[widgetId]
+          for (const instanceId of matchingInstanceIds) {
+            recoilPersist.widgetSettingsState[instanceId] = originalSettings
+          }
+        }
+      }
+    }
+
+    // Save state
     localStorage.setItem('recoil-persist', JSON.stringify(recoilPersist))
   }
 })
