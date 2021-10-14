044cf1771a60294282f9a8754ea73912e4e7f30b
SplittyDev <splittydev@gmail.com>
2021-10-14 23:18:16 +0200

    Export hooks in one place

---
+++ src/hooks/index.js
@@ -0,0 +1,4 @@
+export { default as useInterval } from './useInterval'
+export { useSkeletonLoader } from './useSkeletonLoader'
+export { useWidgetAction } from './useWidgetAction'
+export { useWidgetState } from './useWidgetState'
